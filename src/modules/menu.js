1e0d94b02ae1ff8502c133edbbb5ee3e5d84af3c
joyscript <joyscript7@gmail.com>
2022-07-28 02:30:01 +0300

    implement functions of rendering cards

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,14 +1,62 @@
 export const menu = () => {
-  const restaurant = 'tanuki';
+  const cardsContainer = document.querySelector('.cards-menu');
+  const restaurant = JSON.parse(localStorage.getItem('restaurant'));
 
-  const renderItems = (data) => {
-    data.forEach((item) => {
-      console.log(item);
-    });
+  const updateHeading = (restaurant) => {
+    const { name, stars, price, kitchen } = restaurant;
+
+    document.querySelector('.restaurant-title').textContent = name;
+    document.querySelector('.card-info .rating').textContent = stars;
+    document.querySelector('.card-info .price').textContent = `От ${price} ₽`;
+    document.querySelector('.card-info .category').textContent = kitchen;
   };
 
-  fetch(`./db/${restaurant}.json`)
-    .then((response) => response.json())
-    .then((data) => renderItems(data))
-    .catch((err) => console.log(err.message));
+  const renderCard = (product) => {
+    const { name, description, price, image } = product;
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+      <img src=${image} alt="image" class="card-image" />
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title card-title-reg">${name}</h3>
+        </div>
+        <div class="card-info">
+          <div class="ingredients">${description}</div>
+        </div>
+        <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">${price} ₽</strong>
+        </div>
+      </div>
+      `;
+
+    cardsContainer.append(card);
+  };
+
+  if (restaurant) {
+    updateHeading(restaurant);
+
+    fetch(`https://deliveryfood-b2697-default-rtdb.firebaseio.com/db/${restaurant.products}`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          cardsContainer.innerHTML = `
+            <h3 class="card-title">Извините, невозможно отобразить продукты.<br>
+            Ошибка ${res.status} - ${res.statusText}</h3>
+          `;
+          throw new Error(`error: ${res.status} ${res.statusText}`);
+        }
+      })
+      .then((data) => data.forEach((product) => renderCard(product)))
+      .catch((err) => console.log(err.message));
+  } else {
+    window.location = 'index.html';
+  }
 };
--- src/modules/partners.js
+++ src/modules/partners.js
@@ -1,10 +1,49 @@
 export const partners = () => {
-  const renderItems = (data) => {
-    data.forEach((item) => console.log(item));
+  const cardsContainer = document.querySelector('.cards-restaurants');
+
+  const renderCard = (restaurant) => {
+    const { name, time_of_delivery, stars, price, kitchen, image, products } = restaurant;
+
+    const card = document.createElement('a');
+    card.classList.add('card', 'card-restaurant');
+    card.href = 'restaurant.html';
+
+    card.innerHTML = `
+      <img src=${image} alt="image" class="card-image" />
+        <div class="card-text">
+          <div class="card-heading">
+            <h3 class="card-title">${name}</h3>
+            <span class="card-tag tag">${time_of_delivery} мин</span>
+          </div>
+          <div class="card-info">
+            <div class="rating">${stars}</div>
+            <div class="price">От ${price} ₽</div>
+            <div class="category">${kitchen}</div>
+          </div>
+        </div>
+      `;
+
+    cardsContainer.append(card);
+
+    card.addEventListener('click', (e) => {
+      e.preventDefault();
+      localStorage.setItem('restaurant', JSON.stringify(restaurant));
+      window.location = 'restaurant.html';
+    });
   };
 
   fetch('https://deliveryfood-b2697-default-rtdb.firebaseio.com/db/partners.json')
-    .then((response) => response.json())
-    .then((data) => renderItems(data))
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        cardsContainer.innerHTML = `
+          <h3 class="card-title">Извините, невозможно отобразить данные.<br>
+          Ошибка ${res.status} - ${res.statusText}</h3>
+        `;
+        throw new Error(`error: ${res.status} ${res.statusText}`);
+      }
+    })
+    .then((data) => data.forEach((restaurant) => renderCard(restaurant)))
     .catch((err) => console.log(err.message));
 };
