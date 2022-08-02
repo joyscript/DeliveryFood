7571edb3ad823e1914b695a4162b082589b04d28
joyscript <joyscript7@gmail.com>
2022-08-02 04:30:02 +0300

    add customer info

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -2,22 +2,31 @@ import { products } from './menu';
 
 export const cart = () => {
   const cardsContainer = document.querySelector('.cards-menu');
+  const inputAddress = document.querySelector('.input-address');
   const buttonCart = document.querySelector('.button-cart');
   const modalCart = document.querySelector('.modal-cart');
+  const modalInfo = document.querySelector('.modal-info');
+  const infoForm = document.querySelector('#infoForm');
+
   const modalBody = modalCart.querySelector('.modal-body');
-  const modalHeader = modalCart.querySelector('.modal-header');
-  const modalFooter = modalCart.querySelector('.modal-footer');
   const modalSum = modalCart.querySelector('.modal-pricetag');
-  const clearBtn = modalCart.querySelector('.clear-cart');
-  const sendBtn = modalCart.querySelector('.button-send');
+  const orderBtn = modalCart.querySelector('.button-order');
+  const clearCartBtn = modalCart.querySelector('.button-clear-cart');
+  const clearInfoBtn = modalInfo.querySelector('.button-clear-info');
+  const infoName = infoForm.querySelector('#info-login');
+  const infoPhone = infoForm.querySelector('#info-phone');
+  const infoAddress = infoForm.querySelector('#info-address');
+  const infoTime = infoForm.querySelector('#info-time');
+
   const maxQuantity = 10;
   let cartArr = [];
+  let customer = {};
 
-  const openModalCart = () => modalCart.classList.add('open');
+  const openModal = (modal) => modal.classList.add('open');
 
-  const closeModalCart = (e) => {
-    if (e.target === modalCart || e.target.classList.contains('close')) {
-      modalCart.classList.remove('open');
+  const closeModal = (e, modal) => {
+    if (e.target === modal || e.target.classList.contains('close')) {
+      modal.classList.remove('open');
     }
   };
 
@@ -32,11 +41,15 @@ export const cart = () => {
     localStorage.setItem('cart', JSON.stringify(cartArr));
   };
 
+  const saveInfo = () => {
+    localStorage.setItem('customer', JSON.stringify(customer));
+  };
+
   const clearCart = () => {
     cartArr = [];
     modalBody.innerHTML = '';
     modalSum.textContent = `0 ₽`;
-    sendBtn.classList.remove('open');
+    orderBtn.classList.remove('open');
     modalCart.classList.remove('open');
     localStorage.removeItem('cart');
   };
@@ -66,21 +79,46 @@ export const cart = () => {
       modalBody.append(foodRow);
     });
 
-    sendBtn.classList.add('open');
-
+    orderBtn.classList.add('open');
     calculateSum();
   };
 
+  const renderModalInfo = () => {
+    infoName.value = customer.name || '';
+    infoPhone.value = customer.phone || '';
+    infoAddress.value = customer.address || '';
+    infoTime.value = customer.time || '';
+  };
+
+  const getUserInfo = () => {
+    customer = {
+      name: infoName.value,
+      phone: infoPhone.value,
+      address: infoAddress.value,
+      time: infoTime.value,
+    };
+
+    saveInfo();
+  };
+
   const showMessage = (message) => {
-    modalBody.innerHTML = `<h2 class="card-title">${message}</h2>`;
-    modalHeader.classList.add('invisible');
-    modalFooter.classList.add('invisible');
+    modalInfo.classList.remove('open');
+
+    const modalMessage = document.createElement('div');
+    modalMessage.classList.add('modal', 'modal-message', 'open');
+
+    modalMessage.innerHTML = `
+      <div class="modal-dialog">
+        <h2 class="modal-title">${message}</h2>
+      </div>
+    </div>`;
+
+    document.body.append(modalMessage);
     document.body.classList.add('lock');
 
     setTimeout(() => {
       clearCart();
-      modalHeader.classList.remove('invisible');
-      modalFooter.classList.remove('invisible');
+      modalMessage.remove();
       document.body.classList.remove('lock');
     }, 2000);
   };
@@ -92,6 +130,11 @@ export const cart = () => {
     renderModalCart();
   }
 
+  if (localStorage.getItem('customer')) {
+    customer = JSON.parse(localStorage.getItem('customer'));
+    renderModalInfo();
+  }
+
   if (cardsContainer) {
     cardsContainer.addEventListener('click', (e) => {
       if (e.target.closest('.button-add-cart')) {
@@ -100,14 +143,28 @@ export const cart = () => {
         addToCart(btn.dataset.id);
         saveCart();
         renderModalCart();
-        openModalCart();
+        openModal(modalCart);
       }
     });
   }
 
-  buttonCart.addEventListener('click', openModalCart);
-  modalCart.addEventListener('click', closeModalCart);
-  clearBtn.addEventListener('click', clearCart);
+  inputAddress.addEventListener('change', () => {
+    customer.address = inputAddress.value;
+    saveInfo();
+    renderModalInfo();
+  });
+
+  buttonCart.addEventListener('click', () => {
+    openModal(modalCart);
+  });
+
+  modalCart.addEventListener('click', (e) => {
+    closeModal(e, modalCart);
+  });
+
+  modalInfo.addEventListener('click', (e) => {
+    closeModal(e, modalInfo);
+  });
 
   modalBody.addEventListener('click', (e) => {
     if (e.target.classList.contains('counter-button')) {
@@ -127,16 +184,31 @@ export const cart = () => {
     }
   });
 
-  sendBtn.addEventListener('click', () => {
+  orderBtn.addEventListener('click', (e) => {
+    modalCart.classList.remove('open');
+    openModal(modalInfo);
+  });
+
+  clearCartBtn.addEventListener('click', clearCart);
+
+  clearInfoBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    infoForm.reset();
+  });
+
+  infoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    getUserInfo();
+
     fetch('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
-      body: localStorage.getItem('cart'),
+      body: JSON.stringify({ customer: customer, order: cartArr }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
     })
       .then((res) => {
-        if (res.ok) showMessage('Спасибо за заказ! Мы скоро с вами свяжемся.');
+        if (res.ok) showMessage('Спасибо за заказ! <br>Мы скоро с вами свяжемся.');
       })
       .catch((err) => {
         showMessage('Извините, невозможно оформить заказ');
